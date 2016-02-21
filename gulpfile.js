d91a5c2137cf341fbbbdbd24692804f5daf5a797
Jenny Swift <cheezyspaghetti@gmail.com>
2016-02-21 14:21:53 +1100

    Setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('style.scss');
+
+    mix.scripts([
+        'plugins/*.js',
+        'config.js',
+        'repositories/**/*.js',
+        'components/**/*.js',
+        'app.js',
+    ], 'public/js/all.js');
+
+    mix.version(["css/style.css", "js/all.js"]);
+    mix.stylesIn('resources/assets/css', 'public/css/plugins.css');
 });
+
+
---
+++ resources/assets/js/app.js
@@ -0,0 +1,26 @@
+//var App = Vue.component('app', {
+//
+//});
+//
+//var router = new VueRouter({
+//    hashbang: false
+//});
+//
+//router.map({
+//    '/': {
+//        component: EntriesPage,
+//        subRoutes: {
+//            //default for if no id is specified
+//            '/': {
+//                component: Item
+//            },
+//            '/:id': {
+//                component: Item
+//            }
+//        }
+//    }
+//});
+//
+//router.start(App, 'body');
+//
+//
